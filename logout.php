a125d0fdba5358d4127da497c0bc61e568fc346d
araarq <aaq8@njit.edu>
2018-12-11 12:44:22 -0500

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+echo"logged out";
+
+
+session_unset(); 
+session_destroy(); 
+echo '<form method="post" action="">';
+		       
+echo' <input name="BtH" type="submit" value="Back to Home" />';
+echo '</form>';
+
+if(isset($_POST['BtH'])){
+	header('Location: http://localhost/login.php');
+}
+
+?>
